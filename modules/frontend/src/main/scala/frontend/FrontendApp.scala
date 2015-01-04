a630143dbbd9a5086f8613bc3893d77ee7a6c91a
Nikolay Kushin <zikolach@gmail.com>
2015-01-04 16:19:42 +0100

    init

---
+++ modules/frontend/src/main/scala/frontend/FrontendApp.scala
@@ -0,0 +1,11 @@
+package frontend
+
+import scala.scalajs.js
+
+object FrontendApp extends js.JSApp {
+
+  def main(): Unit = {
+    println("aaaaaaa")
+  }
+
+}
---
+++ modules/shared/src/main/scala/shared/Common.scala
@@ -0,0 +1,5 @@
+package shared
+
+object Common {
+  val test = "Test String"
+}
